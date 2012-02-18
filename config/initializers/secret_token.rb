db32c9ccb0f08dcc98d4d7c0e953c33802c38d57
Kate Ray <email@kateray.net>
2012-02-18 16:43:45 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackerolympics::Application.config.secret_token = '9556a4b616224367cb3067028cbfb56c5464996f51d9fbb016c82ef5b9d339c0376c5043a117885f474e7c6d31d6a209f11d91e083148a280bff7347b684cd34'
