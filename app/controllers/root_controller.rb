b1ac09b837ad0f5188726dce2bfe9555e074c682
Mike Caprio <info@brainewave.com>
2012-02-18 17:08:02 -0500

    Changed the search terms in Flickr search

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -3,6 +3,6 @@ class RootController < ApplicationController
   def home
     FlickRaw.api_key="7e2cf59f1f4b34e9f9fadf3db63b270d"
     FlickRaw.shared_secret="0ef4a87215ab8428"
-    @smiles = FlickRaw::Flickr::Photos.search(:tags => 'smile')
+    @smiles = FlickRaw::Flickr::Photos.search(:tags => 'dog smile')
   end
 end
